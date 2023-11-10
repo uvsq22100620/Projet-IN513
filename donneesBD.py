e8b0add05486031f5562c3d1a8c15a1f953bc700
Camille LE CORRE <camille.le-corre@ens.uvsq.fr>
2023-11-10 17:15:04 +0100

    maj

--- donneesBD.py
+++ donneesBD.py
@@ -9,13 +9,36 @@ num_entrees = random.randint(1, 11)
 num_plats = random.randint(12, 28)
 num_desserts = random.randint(29, 45)
 
-liste_noms_clients = ['NULL', 'Auclair', 'Barth', 'Bouleau', 'Bourra', 'Brezellec', 'Camion', 'Chairet', 'Chevalier', 'Corsi', 'Coucheney', 'Cremazy', 'Debat', 'Diab', 'Durand',
-                          'Etchebest', 'Fages', 'Ferrat', 'Finance', 'Fourneau', 'Gattoliat', 'Garnier', 'Gaumer', 'Gianfrotta', 'Hoareau', 'Jacquin', 'Jellouli', 'Keddis', 'Lamy', 'Lebbah',
-                          'Le Corre', 'Lefevre', 'Lemaire', 'Lignac', 'Manour', 'Marque', 'Mayer', 'Mediouni', 'Moindjie', 'Moreno', 'Mothor', 'Netter',
+liste_noms_clients = ['NULL', 'Auclair', 'Barth', 'Berdrate', 'Bouleau', 'Bourra', 'Brezellec', 'Camion', 'Chairet', 'Chalant', 'Chevalier', 'Corsi', 'Coucheney', 'Cremazy', 'Debat', 'Diab', 'Durand',
+                          'Etchebest', 'Fages', 'Farny', 'Ferrat', 'Finance', 'Fourneau', 'Gattoliat', 'Garnier', 'Gaumer', 'Gianfrotta', 'Hoareau', 'Jacquin', 'Jellouli', 'Juliza', 'Keddis', 'Lamy', 'Lebbah',
+                          'Le Corre', 'Lefevre', 'Lemaire', 'Lignac', 'Manour', 'Marque', 'Mayer', 'Mediouni', 'Mercorelli', 'Moindjie', 'Moreno', 'Mothor', 'Netter', 'Ninoux',
                           'Prehaud', 'Pret', 'Prieu', 'Quessette', 'Rincheval', 'Rotella', 'Sandu', 'Sapriel', 'Sitterlin', 'Sourdeval', 'Strozecki', 'Szuplewzki', 'Taher',
-                          'Terki', 'Timsiline', 'Tseveendorj', 'Vial', 'Vincens']
+                          'Terki', 'Thibuta', 'Timsiline', 'Tseveendorj', 'Vial', 'Vincens']
 
 liste_num_serveurs = [k for k in range(0,11)]
+
+def serveurs_travaillant_sachant_service(jour, annee):
+    """ Retourne la liste des serveurs travaillant le jour et l'annnée correspondante"""
+    if (jour=='Ven') or (jour=='Sam') or (jour=='Dim'):
+        nb_serveurs = random.choices([2, 3, 4], [0.25, 0.7, 0.05])[0]
+    else:
+        nb_serveurs = random.choices([1, 2, 3], [0.2, 0.75, 0.05])[0]
+
+    if annee==2023:      #attention int/str
+        serveurs = [k for k in range(2,11)]
+    elif annee==2022:
+        serveurs = [k for k in range(3, 9)]
+    elif annee==2021:
+        serveurs = [k for k in range(0,9)]
+    else:
+        serveurs = [k for k in range(0,7)]
+    
+    while len(serveurs)!= nb_serveurs:
+        serveurs.remove(random.choices(serveurs)[0])
+
+    return serveurs
+
+#print(serveurs_travaillant_sachant_service('Mar', 2023))
 
 def commandes_semaine():
 
@@ -40,46 +63,28 @@ def commandes_semaine():
      
     liste_services2 = [2, 3, 5]
     fic_commandes = open('commandes.txt', 'w')
+    proba_clients = [0.8]+[(0.2/len(liste_noms_clients)) for k in range(len(liste_noms_clients)-1)]
 
-    for num_service in range(len(liste_services2)):      # pour chaque service
-        for num_client in range(liste_services2[num_service]):       # pour chaque commande
-            nom_client = random.choice(liste_noms_clients, [0.8]+[0.1 for k in range(len(liste_noms_clients)-1)])[0]
+    for num_service in range(len(liste_services)):      # pour chaque service
+        for num_client in range(liste_services[num_service]):       # pour chaque commande
+            nom_client = random.choices(liste_noms_clients, proba_clients)[0]
             if nom_client != 'NULL':
                 nom_client = "'" + nom_client + "'"
             jour = 0
             mois = 0
             annee = 0
-            date_commande = 0
+            date_commande = 'date'
             service = "'" + l_services[num_service%2] + "'"
-            num_serveur = random.choice(serveurs_travaillant_sachant_service(jour, annee))
+            num_serveur = random.choices(serveurs_travaillant_sachant_service(jour, annee))[0]
             num_table = 0
             tuple_commande = "(" + str(num_commande) + ", " + nom_client + ", " + str(date_commande) + ", " + service + ", " + str(num_serveur) + ", " + str(num_table) + ")"
-            fic_commandes.write(tuple_commande)
+            fic_commandes.write(tuple_commande+'\n')
             num_commande += 1
     fic_commandes.close()
 
 commandes_semaine()
 
-def serveurs_travaillant_sachant_service(jour, annee):
-    """ Retourne la liste des serveurs travaillant le jour et l'annnée correspondante"""
-    if (jour=='Ven') or (jour=='Sam') or (jour=='Dim'):
-        nb_serveurs = random.choice([2, 3, 4], [0.25, 0.7, 0.05])[0]
-    else:
-        nb_serveurs = random.choice([1, 2, 3], [0.2, 0.75, 0.05])[0]
 
-    if annee==2023:      #attention int/str
-        serveurs = [k for k in range(2,11)]
-    elif annee==2022:
-        serveurs = [k for k in range(3, 9)]
-    elif annee==2021:
-        serveurs = [k for k in range(0,9)]
-    else:
-        serveurs = [k for k in range(0,7)]
-    
-    while len(serveurs)!= nb_serveurs:
-        serveurs.remove(random.choice(serveurs))[0]
-
-    return serveurs
 
 # pour les boissons, mettre souvent de l'eau
 
@@ -103,4 +108,4 @@ def tuples_COMMANDES(liste_services):
             ind_cl += 1
 
 
-# chaque commande a au moins une boisson associée
+# chaque commande a au moins une boisson associée ou un EPD
